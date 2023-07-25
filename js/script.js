9aa8bb1e6b9f905a3155f10cbaa6b87d413b3b3f
miura0812 <takahiromiura.19890812@gmail.com>
2023-07-25 10:43:59 +0900

    aaaaaaa

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
-// $(function(){
-//   $(".slide-items").slick({
-//     autoplay:true,
-//     slidesToShow:2,
-//     infinite:true,
-//     slidesToScroll:1,
-//     centerMode: true,
-//   });
-//   });
+
+$(function(){
+  //読み込みが完了したら実行する
+  $(window).on('load',function(){
+    //ローディングアニメーションをフェードアウト
+    $('.loader').delay(600).fadeOut(600);
+    //背景色をフェードアウト
+    $('.loader-bg').delay(900).fadeOut(800);
+});
+
+  //ページの読み込みが完了してなくても5秒後にアニメーションを非表示にする
+  setTimeout(function(){
+    $('.loader-bg').fadeOut(600);
+  },5000);
+});
 
 
 const btn = document.querySelector('.menu-btn');
